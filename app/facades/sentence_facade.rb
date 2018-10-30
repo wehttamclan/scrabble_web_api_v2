1b58c81f7f9a98a274d08c56289a6ae15890ca47
Matthew Peters <JiveTurkey@MattBook-Pro.local>
2018-10-30 10:07:32 -0600

    Made a connection.

--- app/facades/sentence_facade.rb
+++ app/facades/sentence_facade.rb
@@ -6,10 +6,19 @@ class SentenceFacade
   end
 
   def examples
-
+    byebug
   end
 
-  def service
-    Faraday.get("https://od-api.oxforddictionaries.com/api/v1/entries/en/#{word}/sentences")
+  def response
+    conn.get("/api/v1/entries/en/#{word}/sentences")
+  end
+
+  def conn
+    Faraday.new(url: "https://od-api.oxforddictionaries.com") do |faraday|
+      faraday.headers['Content-Type'] = 'application/json'
+      faraday.headers['app_id'] = ENV['oxford_app_id']
+      faraday.headers['app_key'] = ENV['oxford_app_key']
+      faraday.adapter  Faraday.default_adapter
+    end
   end
 end
