85af6cd97dee522c5a16c4cd8d4e4b00b334ade4
Matthew Peters <JiveTurkey@MattBook-Pro.local>
2018-10-30 09:41:58 -0600

    Creates sentence examples controller.

---
+++ app/controllers/sentence_examples_controller.rb
@@ -0,0 +1,2 @@
+class SentenceExamplesController < ApplicationController
+end
--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -1,18 +1,20 @@
 require 'rails_helper'
 
-describe 'stuff' do
-  # As a guest user (no sign in required)
-  # When I visit "/"
-  visit '/'
-  # And I fill in a text box with "mindfulness"
-  fill_in 'text box', with: 'mindfulness'
-  # And I click "Submit"
-  click_on 'Submit'
-  # Then I should see a message that says "Examples for using 'mindfulness'"
-  within(first(".example")) do
-    expect(current_page).to have_content "Examples for using 'mindfulness'"
-    # And I should see a list of sentences with examples of how to use the word
-    # And I should see only sentences where the region for usage is "Brittish" or "Canadian"
-    # And I should not see sentences for any other regions (or blank regions)
+describe 'Examples' do
+  it 'returns a list of example sentences' do
+    # As a guest user (no sign in required)
+    # When I visit "/"
+    visit '/'
+    # And I fill in a text box with "mindfulness"
+    fill_in 'word', with: 'mindfulness'
+    # And I click "Submit"
+    click_on 'Submit'
+    # Then I should see a message that says "Examples for using 'mindfulness'"
+    within(first(".example")) do
+      expect(current_page).to have_content "Examples for using 'mindfulness'"
+      # And I should see a list of sentences with examples of how to use the word
+      # And I should see only sentences where the region for usage is "Brittish" or "Canadian"
+      # And I should not see sentences for any other regions (or blank regions)
+    end
   end
 end
